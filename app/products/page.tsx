ca7312574c3a07034e0d9f7e5d540bcad1d7093a
AnaSilvaLuiza <ana.silva@vortal.biz>
2025-02-11 06:53:04 +0000

    chore: added migrations

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,24 @@
-const Products = () => {};
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { PlusIcon } from "lucide-react";
 
-export default Products;
+export default async function ProductsPage() {
+  const products = await db.product.findMany();
+
+  return (
+    <div className="w-full space-y-8 p-8">
+      <div className="item-center flex w-full justify-between">
+        <div className="space-y-1">
+          <span className="text-xs font-semibold text-slate-500">
+            Gestão de Produtos
+          </span>
+          <h2 className="text-xl font-semibold">Products</h2>
+        </div>
+        <Button className="gap-2">
+          <PlusIcon size={20} />
+          New Product
+        </Button>
+      </div>
+    </div>
+  );
+}
