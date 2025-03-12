7f893df3631b4ca2929ea9f1c0126529bbb640c0
AnaSilva <ana.silva@vortal.biz>
2025-03-12 16:09:58 +0000

    feat: added dynamic route handles

---
+++ app/api/products/[id]/route.ts
@@ -0,0 +1,25 @@
+import { db } from "@/app/_lib/prisma";
+import { NextRequest } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("teste");
+  console.log({ query });
+  const productId = params.id;
+  const product = await db.product.findUnique({ where: { id: productId } });
+
+  if (!product)
+    return Response.json({ message: "Product not found" }, { status: 404 });
+
+  return Response.json(product, { status: 200 });
+}
+
+export async function DELETE({ params }: { params: { id: string } }) {
+  const productId = params.id;
+  const product = await db.product.delete({ where: { id: productId } });
+
+  return Response.json(product, { status: 204 });
+}
